b85c836effea5181f840ebc4ea5f320e905a17b0
Marek Magdziak <marek.magdziak@hotmail.cz>
2018-11-03 14:55:20 +0100

    articles & deployment

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const path = require('path')
 const fs = require('fs')
 const PORT = process.env.PORT || 5000
 
-express()
+const articles = JSON.parse(fs.readFileSync(path.join(__dirname, './content/articles.json')).toString())
+
+let server = express()
   .use('/public', express.static(path.join(__dirname, 'dist', 'public')))
-  .get('/scripts.js', (req, res) => res.send(fs.readFileSync('dist/scripts.js') +''))
+  .get('/scripts.js', (req, res) => res.send(fs.readFileSync('dist/scripts.js').toString()))
   .get('/styles.css', (req, res) => {
     res.set('Content-Type', 'text/css');
     res.write(fs.readFileSync('dist/styles.css') +'');
     res.end();
   })
-  .get('/', (req, res) => res.send(fs.readFileSync('dist/index.html') +''))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.send(fs.readFileSync('dist/index.html') +''));
+
+for (const article of articles) {
+  server = server.get(`/${article.id}`, (req, res) => res.send(fs.readFileSync(`dist/${article.id}/index.html`).toString()));
+}
+
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- tools/build-html.js
+++ tools/build-html.js
@@ -1,34 +1,53 @@
-var fs = require('fs');
-var md = require('markdown-it')();
-var path = require('path');
+const fs = require('fs');
+const md = require('markdown-it')();
+const path = require('path');
+const extractSummary = require('extract-summary')
 
-function fromDir(startPath, filter, array){
+const articles = loadArticles();
+CreateFrontPage();
+CreateArticlePages();
 
-    if (!fs.existsSync(startPath)){
-        return;
+function loadArticles() {
+    return JSON.parse(fs.readFileSync(path.join(__dirname, '../content/articles.json')).toString())
+    .map(article => ({
+        id: article.id,
+        name: article.name,
+        date: article.date,
+        content: md.render(fs.readFileSync(path.join(__dirname, `../content/articles/${article.id}/content.md`)).toString())
+    }))
+}
+
+function CreateFrontPage() {
+    let html = '';
+    const articleTemplate = fs.readFileSync(path.join(__dirname, '../src/article-content.html')).toString()
+
+    for (const article of articles) {
+        html += articleTemplate
+            .replace('{{heading}}', article.name)
+            .replace('{{date}}', article.date)
+            .replace('{{content}}', extractSummary(article.content, 'html'))
+            .replace('{{article-url}}', `/${article.id}`)
     }
 
-    var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        var filename=path.join(startPath,files[i]);
-        var stat = fs.lstatSync(filename);
-        if (stat.isDirectory()){
-            fromDir(filename,filter); //recurse
+    const source = fs.readFileSync('src/index.html');
+    fs.writeFileSync('dist/index.html', source.toString().replace('{{content}}', html));
+}
+
+function CreateArticlePages() {
+    const pageTemplate = fs.readFileSync(path.join(__dirname, '../src/article-page.html')).toString()
+
+    for (const article of articles) {
+        const html = pageTemplate
+            .replace('{{heading}}', article.name)
+            .replace('{{date}}', article.date)
+            .replace('{{content}}', article.content);
+
+        const dir = `dist/${article.id}`;
+
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
         }
-        else if (filename.indexOf(filter)>=0) {
-            array.push(filename);
-            console.log('-- found: ',filename);
-        };
-    };
-};
 
-var markdownFiles = [];
-fromDir('src/markdown', '.md', markdownFiles);
-
-var result = markdownFiles.map(file => {
-    var content = fs.readFileSync(file).toString();
-    return md.render(content);
-}).join('</article><article class="page__article">');
-
-var source = fs.readFileSync('src/index.html');
-fs.writeFileSync('dist/index.html', source.toString().replace('{{content}}', result));
+        fs.writeFileSync(`${dir}/index.html`, html);
+    }
+}
